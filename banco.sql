f7574aa0136cc7d34246e195552a85b52d3e6d90
Carlo Moro <cnmoro@gmail.com>
2017-09-11 18:15:56 -0300

    Script sql adicionado

---
+++ banco.sql
@@ -0,0 +1,15 @@
+create database agendaeletronica;
+
+use agendaeletronica;
+
+
+
+CREATE TABLE `agendaeletronica`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `endereço` VARCHAR(255) NOT NULL,
+  `telfixo` DOUBLE NOT NULL,
+  `celular` DOUBLE NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
